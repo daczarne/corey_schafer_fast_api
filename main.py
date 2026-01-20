4a22bef2f331fd9306f2fb0d9b8eb6b58e14f919
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2026-01-20 12:21:39 +0100

    Adds the post page

--- main.py
+++ main.py
@@ -41,6 +41,27 @@ def home(request: Request) -> _TemplateResponse:
     )
 
 
+@app.get(path = "/posts/{post_id}", include_in_schema = False)
+def post_page(request: Request, post_id: int) -> _TemplateResponse:
+    
+    for post in posts:
+        if post.get("id") == post_id:
+            title: str = post["title"][:50]
+            return templates.TemplateResponse(
+                request = request,
+                name = "post.html",
+                context = {
+                    "post": post,
+                    "title": title,
+                },
+            )
+    
+    raise HTTPException(
+        status_code = status.HTTP_404_NOT_FOUND,
+        detail = "Post not found",
+    )
+
+
 @app.get(path = "/api/posts")
 def get_posts() -> list[dict]:
     return posts
