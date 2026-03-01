717497f724a46058a6935859773acb8c1fc17608
Daniel Czarnievicz <16988051+daczarne@users.noreply.github.com>
2026-03-01 02:29:29 +0100

    Updates routers

---
+++ app/routers/posts.py
@@ -0,0 +1,211 @@
+from collections.abc import Sequence
+from typing import Annotated, Any
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import Result, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from app.database import get_db
+from app.models import Post, User
+from app.routers.utils import query_post_by_post_id, query_user_by_user_id
+from app.schemas import PostCreate, PostResponse, PostUpdate
+
+
+router = APIRouter(
+    prefix = "/posts",
+    tags = ["posts"],
+)
+
+
+@router.delete(
+    path = "/{post_id}",
+    status_code = status.HTTP_204_NO_CONTENT,
+    summary = "Delete a post by `post_id`",
+    description = """
+        This endpoint deletes the post with the supplied `post_id`.
+    """,
+    deprecated = False,
+)
+async def delete_post(
+        post_id: int,
+        db: Annotated[AsyncSession, Depends(dependency = get_db)],
+    ) -> None:
+    
+    post: Post | None = await query_post_by_post_id(post_id = post_id, db = db)
+    
+    if not post:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Post not found",
+        )
+    
+    await db.delete(instance = post)
+    await db.commit()
+
+
+@router.get(
+    path = "/",
+    response_model = list[PostResponse],
+    summary = "Get all posts for the wall",
+    description = """
+        This endpoint fetches all posts in the DB.
+    """,
+    deprecated = False,
+)
+async def get_posts(
+        db: Annotated[AsyncSession, Depends(dependency = get_db)],
+    ) -> Sequence[Post]:
+    
+    query_post: Result[tuple[Post]] = await db.execute(
+        statement = select(Post).options(selectinload(Post.author)),
+    )
+    posts: Sequence[Post] = query_post.scalars().all()
+    
+    return posts
+
+
+@router.get(
+    path = "/{post_id}",
+    response_model = PostResponse,
+    summary = "Get a post by `post_id`",
+    description = """
+        This endpoint returns the post with the supplied `post_id`.
+    """,
+    deprecated = False,
+)
+async def get_post(
+        post_id: int,
+        db: Annotated[AsyncSession, Depends(dependency = get_db)],
+    ) -> Post:
+    
+    post: Post | None = await query_post_by_post_id(post_id = post_id, db = db)
+    
+    if not post:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Post not found",
+        )
+    
+    return post
+
+
+@router.patch(
+    path = "/{post_id}",
+    response_model = PostUpdate,
+    summary = "Update a post by `post_id` (partial update)",
+    description = """
+        This endpoint updates the supplied attributes from the post with the supplied `post_id` (partial update).
+    """,
+    deprecated = False,
+)
+async def update_post_partial(
+        post_id: int,
+        post_data: PostUpdate,
+        db: Annotated[AsyncSession, Depends(dependency = get_db)],
+    ) -> Post:
+    
+    post: Post | None = await query_post_by_post_id(post_id = post_id, db = db)
+    
+    if not post:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Post not found",
+        )
+    
+    update_data: dict[str, Any] = post_data.model_dump(exclude_unset = True)
+    
+    for field, value in update_data.items():
+        setattr(post, field, value)
+    
+    await db.commit()
+    await db.refresh(
+        instance = post,
+        attribute_names = ["author"],
+    )
+    
+    return post
+
+
+@router.post(
+    path = "/",
+    response_model = PostResponse,
+    status_code = status.HTTP_201_CREATED,
+    summary = "Create a new post",
+    description = """
+        This endpoint creates a new post.
+    """,
+    deprecated = False,
+)
+async def create_post(
+        post: PostCreate,
+        db: Annotated[AsyncSession, Depends(dependency = get_db)],
+    ) -> Post:
+    
+    user: User | None = await query_user_by_user_id(user_id = post.user_id, db = db)
+    
+    if not user:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User not found",
+        )
+    
+    new_post: Post = Post(
+        title = post.title,
+        content = post.content,
+        user_id = post.user_id,
+    )
+    
+    db.add(instance = new_post)
+    await db.commit()
+    await db.refresh(
+        instance = new_post,
+        attribute_names = ["author"],
+    )
+    
+    return new_post
+
+
+@router.put(
+    path = "/{post_id}",
+    response_model = PostUpdate,
+    summary = "Update a post by `post_id` (full update)",
+    description = """
+        This endpoint updates all fields in the post with the supplied `post_id` (full update).
+    """,
+    deprecated = False,
+)
+async def update_post_full(
+        post_id: int,
+        post_data: PostCreate,
+        db: Annotated[AsyncSession, Depends(dependency = get_db)],
+    ) -> Post:
+    
+    post: Post | None = await query_post_by_post_id(post_id = post_id, db = db)
+    
+    if not post:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Post not found",
+        )
+    
+    if post_data.user_id != post.user_id:
+        user: User | None = await query_user_by_user_id(user_id = post_data.user_id, db = db)
+        
+        if not user:
+            raise HTTPException(
+                status_code = status.HTTP_404_NOT_FOUND,
+                detail = "User not found",
+            )
+    
+    post.title = post_data.title
+    post.content = post_data.content
+    post.user_id = post_data.user_id
+    
+    await db.commit()
+    await db.refresh(
+        instance = post,
+        attribute_names = ["author"],
+    )
+    
+    return post
